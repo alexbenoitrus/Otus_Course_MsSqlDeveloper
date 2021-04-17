9834b4f5fce51e48b6f4ca6a106c5ced4a394106
benJkee <alex.benoit@ya.ru>
2021-04-17 14:34:01 +0300

    0103 Complete

--- Modules/01/03/hw_subqueries-1801-0eecd8.sql
+++ Modules/01/03/hw_subqueries-1801-0eecd8.sql
@@ -30,14 +30,83 @@ USE WideWorldImporters
 Продажи смотреть в таблице Sales.Invoices.
 */
 
-TODO: напишите здесь свое решение
+DECLARE @SalesDay DATE = '20150704'
+;
+
+
+SELECT
+	p.[PersonID]     AS [PersonID]
+	, p.[FullName]   AS [FullName]
+FROM
+	Application.People p WITH (NOLOCK)
+WHERE 1=1
+	AND p.[IsSalesperson] = 1
+	AND p.[PersonID] != ALL 
+		(
+			SELECT DISTINCT
+				i.[SalespersonPersonID]
+			FROM
+				Sales.Invoices i WITH (NOLOCK)
+			WHERE 1=1
+				AND i.[SalespersonPersonID] IS NOT NULL
+				AND i.[InvoiceDate] = @SalesDay
+		)
+;
+
+
+WITH SalesPersonByDayCTE AS
+(
+	SELECT DISTINCT
+		i.[SalespersonPersonID] AS ID
+	FROM
+		Sales.Invoices i WITH (NOLOCK)
+	WHERE 1=1
+		AND i.[SalespersonPersonID] IS NOT NULL
+		AND i.[InvoiceDate] = @SalesDay
+)
+SELECT
+	p.[PersonID]     AS [PersonID]
+	, p.[FullName]   AS [FullName]
+FROM
+	Application.People p WITH (NOLOCK)
+WHERE 1=1
+	AND p.[IsSalesperson] = 1
+	AND p.[PersonID] != ALL (SELECT [ID] FROM SalesPersonByDayCTE)
+;
+
+-- ------------------------------------------------------------------------------------------------------------------------------------------------------
 
 /*
 2. Выберите товары с минимальной ценой (подзапросом). Сделайте два варианта подзапроса. 
 Вывести: ИД товара, наименование товара, цена.
 */
 
-TODO: напишите здесь свое решение
+SELECT
+	si.[StockItemID]       AS [StockItemID]
+	, si.[StockItemName]   AS [StockItemName]
+	, si.[UnitPrice]       AS [UnitPrice] 
+FROM
+	Warehouse.StockItems si WITH (NOLOCK)
+WHERE
+	si.[UnitPrice] = (SELECT MIN([UnitPrice]) FROM Warehouse.StockItems)
+;
+
+
+WITH MinimalPriceCTE (Amount) AS  
+(
+	SELECT MIN([UnitPrice]) AS Amount FROM Warehouse.StockItems
+)
+SELECT
+	si.[StockItemID]       AS [StockItemID]
+	, si.[StockItemName]   AS [StockItemName]
+	, si.[UnitPrice]       AS [UnitPrice] 
+FROM
+	Warehouse.StockItems si WITH (NOLOCK)
+WHERE
+	si.[UnitPrice] = (SELECT [Amount] FROM MinimalPriceCTE)
+;
+
+-- ------------------------------------------------------------------------------------------------------------------------------------------------------
 
 /*
 3. Выберите информацию по клиентам, которые перевели компании пять максимальных платежей 
@@ -45,7 +114,51 @@ TODO: напишите здесь свое решение
 Представьте несколько способов (в том числе с CTE). 
 */
 
-TODO: напишите здесь свое решение
+DECLARE @TopSize INT = 5
+;
+
+
+SELECT
+	c.[CustomerID]      AS [ID]
+	, c.[CustomerName]  AS [Name]
+	, c.[WebsiteURL]    AS [URL]
+	, c.[CreditLimit]   AS [CreditLimit]
+FROM
+	Sales.Customers c WITH (NOLOCK)
+WHERE 1=1
+	AND c.[CustomerID] IN 
+		(
+			SELECT TOP (@TopSize) 
+				cs.[CustomerID] 
+			FROM 
+				Sales.CustomerTransactions cs WITH (NOLOCK)
+			ORDER BY
+				cs.[TransactionAmount] DESC
+		)
+;
+
+
+WITH TopInvoiceCustomersCTO (ID) AS
+(
+	SELECT TOP (@TopSize) 
+		cs.[CustomerID] AS [ID]
+	FROM 
+		Sales.CustomerTransactions cs WITH (NOLOCK)
+	ORDER BY
+		cs.[TransactionAmount] DESC
+)
+SELECT
+	c.[CustomerID]      AS [ID]
+	, c.[CustomerName]  AS [Name]
+	, c.[WebsiteURL]    AS [URL]
+	, c.[CreditLimit]   AS [CreditLimit]
+FROM
+	Sales.Customers c WITH (NOLOCK)
+WHERE 1=1
+	AND c.[CustomerID] IN (SELECT [ID] FROM TopInvoiceCustomersCTO)
+;
+
+-- ------------------------------------------------------------------------------------------------------------------------------------------------------
 
 /*
 4. Выберите города (ид и название), в которые были доставлены товары, 
@@ -53,7 +166,49 @@ TODO: напишите здесь свое решение
 который осуществлял упаковку заказов (PackedByPersonID).
 */
 
-TODO: напишите здесь свое решение
+DECLARE @TopRich INT = 3;
+
+
+SELECT DISTINCT
+	c.[CityID]      as [City ID]
+	, c.[CityName]  as [City Name]
+	, p.[FullName]  as [Packager Name]
+FROM
+	Application.Cities c WITH (NOLOCK)
+	LEFT JOIN Sales.Customers csm WITH (NOLOCK)     ON csm.[DeliveryCityID] = c.[CityID]
+	LEFT JOIN Sales.Invoices i WITH (NOLOCK)        ON i.[CustomerID] = csm.[CustomerID]
+	LEFT JOIN Sales.InvoiceLines il WITH (NOLOCK)   ON il.[StockItemID] = i.[InvoiceID]
+	LEFT JOIN Application.People p WITH (NOLOCK)    ON p.[PersonID] = i.[PackedByPersonID]
+WHERE 1=1
+	AND il.[StockItemID] = ANY (SELECT TOP (@TopRich) si.[StockItemID] AS ID FROM Warehouse.StockItems si ORDER BY si.[UnitPrice] DESC)
+;
+
+
+WITH TopRichItems (ID) AS 
+(
+    SELECT TOP (@TopRich) 
+        si.[StockItemID]
+    FROM 
+        Warehouse.StockItems si 
+    ORDER BY 
+        si.[UnitPrice] DESC
+)
+SELECT DISTINCT
+	c.[CityID]      as [City ID]
+	, c.[CityName]  as [City Name]
+	, p.[FullName]  as [Packager Name]
+FROM
+	Application.Cities c WITH (NOLOCK)
+	LEFT JOIN Sales.Customers csm WITH (NOLOCK)    ON csm.[DeliveryCityID] = c.[CityID]
+	LEFT JOIN Sales.Invoices i WITH (NOLOCK)       ON i.[CustomerID] = csm.[CustomerID]
+	LEFT JOIN Sales.InvoiceLines il WITH (NOLOCK)  ON il.[StockItemID] = i.[InvoiceID]
+	LEFT JOIN Application.People p WITH (NOLOCK)   ON p.[PersonID] = i.[PackedByPersonID]
+WHERE 1=1
+	AND il.[StockItemID] = ANY (SELECT * FROM TopRichItems)
+;
+
+
+-- ------------------------------------------------------------------------------------------------------------------------------------------------------
 
 -- ---------------------------------------------------------------------------
 -- Опциональное задание
@@ -92,4 +247,62 @@ ORDER BY TotalSumm DESC
 
 -- --
 
-TODO: напишите здесь свое решение
+/*
+
+    Запрос выводит суммы по выставленным счетам и относящимся к ним заказам,
+    если сумма выставаленного счёт больше, чем указанное значение. 
+
+*/
+
+DECLARE @MinimalInvoiceSumm INT = 27000;
+
+DROP TABLE IF EXISTS #InvoicesSumm;
+CREATE TABLE #InvoicesSumm 
+(
+    [InvoiceId] INT
+    , [InvoiceSumm] INT
+);
+
+INSERT INTO #InvoicesSumm (InvoiceId, InvoiceSumm)
+(
+	SELECT 
+		il.[InvoiceId]
+		, SUM(il.[Quantity] * il.[UnitPrice])
+	FROM 
+		Sales.InvoiceLines il
+	GROUP BY 
+		il.[InvoiceId]
+	HAVING 
+		SUM(il.[Quantity] * il.[UnitPrice]) > @MinimalInvoiceSumm 
+);
+
+WITH CompletedOrdersSumm (ID, Summ) AS
+(
+	SELECT 
+		ol.[OrderID]
+		, SUM(ol.[PickedQuantity] * ol.[UnitPrice])	AS [Summ]
+	FROM 
+		Sales.OrderLines ol
+    WHERE 
+        EXISTS (SELECT 1 FROM Sales.Orders o WHERE o.[OrderId] = ol.[OrderID] AND o.[PickingCompletedWhen] IS NOT NULL)
+	GROUP BY
+		ol.[OrderID]
+)
+SELECT 
+	i.[InvoiceID]         AS [InvoiceID]
+	, i.[InvoiceDate]     AS [InvoiceDate]
+	, p.[FullName]        AS [SalesPersonName]
+	, isum.[InvoiceSumm]  AS [TotalSummByInvoice]
+	, cosum.[Summ]        AS [TotalSummForPickedItems]
+FROM 
+	#InvoicesSumm isum              
+    LEFT JOIN Sales.Invoices i            ON isum.[InvoiceID] = i.[InvoiceID]
+    LEFT JOIN Application.People p        ON p.[PersonID] = i.[SalespersonPersonID]
+    LEFT JOIN CompletedOrdersSumm cosum   ON cosum.[ID] = i.[OrderID]
+ORDER BY 
+	[TotalSummByInvoice] DESC
+
+
+DROP TABLE IF EXISTS #InvoicesSumm;
+
+GO
